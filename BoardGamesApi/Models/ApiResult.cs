44870228b8c9952ca33ee791d80aacfa4256b762
Miroslav Popovic <miroslav.popovic@gmail.com>
2017-09-10 23:37:30 +0200

    Add XML documentation to models

--- BoardGamesApi/Models/ApiResult.cs
+++ BoardGamesApi/Models/ApiResult.cs
@@ -3,14 +3,36 @@ using System.Linq;
 
 namespace BoardGamesApi.Models
 {
+    /// <summary>
+    /// Encapsulates an API result with success flag, errors (if any) and result object.
+    /// </summary>
+    /// <typeparam name="T">Type of the result.</typeparam>
     public class ApiResult<T>
     {
+        /// <summary>
+        /// Gets the API error string or empty string if there are no errors.
+        /// </summary>
         public string Error => string.Join("; ", Errors);
-        public IList<string> Errors { get; set; } = new List<string>();
+
+        /// <summary>
+        /// Gets the error list.
+        /// </summary>
+        public IList<string> Errors { get; } = new List<string>();
+
+        /// <summary>
+        /// Gets the value indicating whether the API call was successful or not.
+        /// </summary>
         public bool IsSuccess => !Errors.Any();
 
+        /// <summary>
+        /// Gets or sets the data object.
+        /// </summary>
         public T Data { get; set; }
 
+        /// <summary>
+        /// Add the error to the error list.
+        /// </summary>
+        /// <param name="error">An error to add to errors list.</param>
         public void AddError(string error)
         {
             Errors.Add(error);
--- BoardGamesApi/Models/PagedApiResult.cs
+++ BoardGamesApi/Models/PagedApiResult.cs
@@ -3,12 +3,31 @@ using System.Collections.Generic;
 
 namespace BoardGamesApi.Models
 {
+    /// <inheritdoc />
+    /// <summary>
+    /// Encapsulates an API result with success flag, errors (if any), result list and paging information.
+    /// </summary>
+    /// <typeparam name="T">Type of the result.</typeparam>
     public class PagedApiResult<T> : ApiResult<IEnumerable<T>>
     {
+        /// <summary>
+        /// Gets or sets the page number.
+        /// </summary>
         public int Page { get; set; }
+
+        /// <summary>
+        /// Gets or sets the page size.
+        /// </summary>
         public int PageSize { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total item count.
+        /// </summary>
         public int TotalCount { get; set; }
 
+        /// <summary>
+        /// Gets the total page count.
+        /// </summary>
         public int TotalPages => (int) Math.Ceiling((double) TotalCount / PageSize);
     }
 }
